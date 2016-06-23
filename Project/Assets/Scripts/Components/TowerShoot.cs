7c06cde22cac8990f4b26466d1b9191793ff82e3
badaueba <alan.develop@outlook.com>
2016-06-23 14:18:36 -0300

    torres atirando

---
+++ Project/Assets/Scripts/Components/TowerShoot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace FATEC.ArcadeSpaceBattle.Components {
+	public class TowerShoot : MonoBehaviour {
+		public float fireRate;
+		public GameObject laser;	
+
+		void Start() {
+			StartCoroutine (Updater ());
+		}
+
+		private IEnumerator Updater () {
+			while (true) {
+				GameObject.Instantiate (laser, this.transform.position, this.transform.rotation);		
+				yield return new WaitForSeconds (fireRate);
+			}
+		}
+	}
+}
+
